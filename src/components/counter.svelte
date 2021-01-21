047b335387e627e6b3c345001373c79a16f8e6cf
Nico Bachner <yo@nicob.dev>
2021-01-21 15:37:36 +0100

    Initial commit

---
+++ src/components/counter.svelte
@@ -0,0 +1,26 @@
+<script>
+	let count = 0;
+
+	const increment = () => {
+		count += 1;
+	};
+</script>
+
+<button on:click={increment}>
+	Clicks: {count}
+</button>
+
+<style>
+	button {
+		padding: 1em 2em;
+		color: #ff3e00;
+		background-color: rgba(255, 62, 0, 0.1);
+		border-radius: 2em;
+		width: 200px;
+		font-variant-numeric: tabular-nums;
+	}
+
+	button:active {
+		background-color: rgba(255, 62, 0, 0.2);
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Counter from '$components/counter.svelte';
+</script>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+	}
+
+	h1 {
+		color: #ff3e00;
+		font-size: 4rem;
+		margin: 4rem auto;
+	}
+</style>
+
+<main>
+	<h1>Hello, World!</h1>
+
+	<Counter />
+</main>
